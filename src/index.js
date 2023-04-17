cd56833c37351af9b1b6238aa67046d393504a56
Rich5656 <richard.k.lowther@gmail.com>
2023-04-16 21:53:30 -0400

    Added media query to navbar, plus javascript to allow for dropdown menu display on small screens.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
+import dropDownToggle from "./scripts/toggleDropDown";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
+dropDownToggle();
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
